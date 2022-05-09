37bb17744609f50bc338bceb0175a572c0914950
Maxime Dupont <maxime.dupont@jamespot.com>
2022-05-09 18:24:28 +0200

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import { LeafletMap } from 'svelte-leafletjs';
+    import type { Map } from 'leaflet';
+
+    let getMap: () => Map;
+</script>
+
+<LeafletMap bind:getMap />
